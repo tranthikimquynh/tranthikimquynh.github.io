db5352fea1b9ddf7db7c68da598d145ed3672262
Minh Trọng Nguyễn <minhnguyen@adnovum.vn>
2023-03-04 22:37:49 +0700

    'init'

---
+++ config.js
@@ -0,0 +1,33 @@
+// 1. Chỉnh sửa các dòng chữ theo mục đích của bạn
+const CONFIG = {
+    introTitle: 'Chị Hương ơi, ',
+    introDesc: `Hôm nay… là ngày 20/10 rồi ấy, Em có lời muốn nói với chị…`,
+    btnIntro: 'OK',
+    title: 'Hôm nay, là ngày Phụ nữ Việt Nam, em chỉ muốn nói là… ',
+    desc: 'Mà đố chị, em định nói gì đấy? 😉 ',
+    btnYes: 'Uhm, để đoán thử ',
+    btnNo: 'Không biết, đoán không ra',
+    question:'Chắc là',
+    reasonPlaceholder: 'Gõ bất kì nội dung gì chị nghĩ vào đây nha...',
+    btnReply: 'Send',
+    reply: 'Chị Hương ơi... Hôm nay là 20/10 rồi ấy, em chúc chị ngày càng trẻ trung, dồi dào sức khoẻ và tràn đầy năng lượng trong cuộc sống.🥳',
+    poem1:'    Hôm nay, đường phố bán bông\n\
+    Rộn ràng ai cũng đóa hồng trên tay\n\
+    Mình đi, bỗng thấy hay hay\n\
+    Dặn lòng tự hỏi hôm nay ngày gì?\n\
+    Thì ra ngày lễ chứ chi\n\
+    Nhìn sang phòng chị mà đâu mất rồi\n\
+    ',
+    poem2:'    Ngồi đây cầm bút vẽ vời\n\
+    Viết thơ chúc chị những lời đẹp tươi \n\
+    Chúc luôn vui vẻ nụ cười\n\
+    Ngày càng thêm trẻ rạng ngời như hoa\n\
+    Chúc cho cuộc sống thuận hòa\n\
+    Ngập tràn năng lượng đong đầy yêu thương.\n\
+    ',
+    mess: 'Ngày 20/10 thật vui nha chị',
+    messTitle: "Gửi Chị Hương",
+    messDesc: 'Chúc chị 20/10 vui vẻ🥳',
+    btnAccept: 'Ok',
+}
+
