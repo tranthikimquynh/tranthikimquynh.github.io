323a7a28e0fdf412f9980162f864b2534310464e
tranthikimquynh <126971562+tranthikimquynh@users.noreply.github.com>
2023-03-04 23:42:52 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,33 +1,33 @@
 // 1. Chỉnh sửa các dòng chữ theo mục đích của bạn
 const CONFIG = {
-    introTitle: 'Chị Hương ơi, ',
-    introDesc: `Hôm nay… là ngày 20/10 rồi ấy, Em có lời muốn nói với chị…`,
+    introTitle: 'Hi, em Quỳnh, ',
+    introDesc: `Hôm nay… là một dịp thật đặt biệt, Anh nhờ gió gửi vài lời đến em…`,
     btnIntro: 'OK',
-    title: 'Hôm nay, là ngày Phụ nữ Việt Nam, em chỉ muốn nói là… ',
-    desc: 'Mà đố chị, em định nói gì đấy? 😉 ',
-    btnYes: 'Uhm, để đoán thử ',
-    btnNo: 'Không biết, đoán không ra',
-    question:'Chắc là',
-    reasonPlaceholder: 'Gõ bất kì nội dung gì chị nghĩ vào đây nha...',
+    title: 'Hôm nay, anh không biết có phải là ngày sinh nhật thật của em không? Nhưng anh nhớ năm ngoái là y ngày này, vẫn như vậy... Anh chỉ muốn nói là… ',
+    desc: 'Mà đố em, anh định nói gì đấy? 😉 ',
+    btnYes: 'Uhm, để em đoán thử ',
+    btnNo: 'Không biết, em đoán không ra...',
+    question:'Nhắm mắt lại và nghĩ vễ điều mình mong muốn lúc này!',
+    reasonPlaceholder: 'Và gõ vào đây những gì mình nghĩ vào đây nha',
     btnReply: 'Send',
-    reply: 'Chị Hương ơi... Hôm nay là 20/10 rồi ấy, em chúc chị ngày càng trẻ trung, dồi dào sức khoẻ và tràn đầy năng lượng trong cuộc sống.🥳',
-    poem1:'    Hôm nay, đường phố bán bông\n\
-    Rộn ràng ai cũng đóa hồng trên tay\n\
-    Mình đi, bỗng thấy hay hay\n\
-    Dặn lòng tự hỏi hôm nay ngày gì?\n\
-    Thì ra ngày lễ chứ chi\n\
-    Nhìn sang phòng chị mà đâu mất rồi\n\
+    reply: 'Hi, em! Sinh nhật tuổi 22 rồi ấy, anh không biết làm sao, nhờ gió gửi lời chúc dến em. Chúc em tuổi mới luôn mỉm cười tươi như đóa hoa ngày xuân, rạng ngời như ánh dương mùa hạ, luôn chan hòa em đềm như nước mùa thu và luôn có ánh lửa hồng của riêng mình sưởi ấm đêm đông lạnh gió. Quan trọng nhất là tràn đầy năng lượng vượt qua khó khăn trong cuộc sống. Sinh nhật thật vui em nhé🥳',
+    poem1:'    Tháng ba ngày bốn là chi\n\
+    Để lòng chợt nhớ điều gì đã quên\n\
+    Rồi thì nhật kí gần bên\n\
+    Lật ra mới biết mình quên cái này?\n\
+    Ngồi đây viết vội điều đây\n\
+   Mang theo câu chúc nhờ mây gửi lời\n\
     ',
-    poem2:'    Ngồi đây cầm bút vẽ vời\n\
-    Viết thơ chúc chị những lời đẹp tươi \n\
-    Chúc luôn vui vẻ nụ cười\n\
-    Ngày càng thêm trẻ rạng ngời như hoa\n\
-    Chúc cho cuộc sống thuận hòa\n\
-    Ngập tràn năng lượng đong đầy yêu thương.\n\
+    poem2:'    Chúc luôn rạng rỡ nụ cười\n\
+    Ngày càng duyên dáng mười mươi vẹn phần \n\
+    Chúc cho hạnh phúc bình an\n\
+    Sang thêm tuổi mới ngập tràn yêu thương\n\
+    Chúc em may mắn thuận đường\n\
+    Thành công cuộc sống tương lai rạng ngời.\n\
     ',
-    mess: 'Ngày 20/10 thật vui nha chị',
-    messTitle: "Gửi Chị Hương",
-    messDesc: 'Chúc chị 20/10 vui vẻ🥳',
+    mess: 'Sinh nhật thật vui nha e,',
+    messTitle: "Gửi em Quỳnh!!",
+    messDesc: 'Cười thật nhiều, tự tin và giữ khí chất mà mình đang có nhé! Happy Birthday🥳',
     btnAccept: 'Ok',
 }
 
